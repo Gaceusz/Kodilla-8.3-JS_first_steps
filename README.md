0fc7f8566b9c7dd94d47957eb4c897f3865fbe9f
Piotrek G <inglorion@op.pl>
2017-05-04 02:53:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Kodilla-8.3-JS_first_steps
---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var name = prompt('Wpisz swoje imię');
+var age = prompt('Wpisz swój wiek');
+
+alert('Witaj, ' + name);
+
+alert('Masz ' + age + ' lat(a), musisz się jeszcze wiele nauczyć');
+
+console.log('Witaj, ' + name);
