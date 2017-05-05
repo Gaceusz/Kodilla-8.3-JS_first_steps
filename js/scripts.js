3dd343cc6a1c62ccfa8bcfdf933d0aa322024e60
Gaceusz <inglorion@op.pl>
2017-05-05 14:49:14 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ var name = prompt('Wpisz swoje imię');
 var age = prompt('Wpisz swój wiek');
 
 alert('Witaj, ' + name);
+// prośba o podanie imienia
 
 alert('Masz ' + age + ' lat(a), musisz się jeszcze wiele nauczyć');
 
